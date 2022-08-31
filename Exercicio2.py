f98e19b0db207dc09aefc2f463ae8b773ad1cdfd
Victor Facundo <victorfacundo19@gmail.com>
2022-08-31 17:59:45 -0300

    Exercício 2
    Neste exercício, o usuário informa um número inteiro positivo, e o programa informa quantos dígitos ele possui. (obs: o programa até o momento consta contando 3 dígitos)

---
+++ Exercicio2.py
@@ -0,0 +1,12 @@
+num = int(input('Informe um número inteiro positivo: '))
+if num <= 9:
+    print('O número tem apenas um digito')
+    
+elif num >=10 and num <=99:
+    print('O número tem apenas dois digitos')
+    
+else:
+    print('O número possui 3 digitos')
+
+
+    
