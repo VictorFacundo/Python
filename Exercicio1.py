a18db79a63188a25c0b9232b2a7b289096e28633
Victor Facundo <victorfacundo19@gmail.com>
2022-08-31 17:56:37 -0300

    Uma calculadora dos números bases
    Atividade sugerida pelo professor.

---
+++ Exercicio1.py
@@ -0,0 +1,19 @@
+print('------------------------ Calculadora ------------------------')
+print('| O programa irá automaticamente montar uma tabela de multiplicação |')
+
+for i in range(1,10+1):
+    for j in range(1,10+1):
+        resultado = i*j 
+        print(f'|{i} x {j} = ', resultado, '|')
+        print('------------------------------')
+
+print('Fim')
+    
+
+
+
+
+
+
+
+
